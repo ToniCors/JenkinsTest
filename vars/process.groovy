6464c749dc49d744a727a1d088808e60279e89f7
ToniCors <tonicors8@gmail.com>
2024-05-18 18:13:54 +0200

    init

--- vars/process.groovy
+++ vars/process.groovy
@@ -1,10 +1,10 @@
 def info(message) {
     echo "INFO: ${message}"
 }
-//
-//def info() {
-//    echo "INFO: "
-//}
+
+def info() {
+    echo "INFO: TEST NO PARAMETER"
+}
 
 def warning(message) {
     echo "WARNING: ${message}"
