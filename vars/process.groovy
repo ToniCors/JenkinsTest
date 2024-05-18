7a6bfdffac60c9e8dacc16e6f72fbb6316b37785
ToniCors <tonicors8@gmail.com>
2024-05-18 18:00:15 +0200

    init

--- vars/process.groovy
+++ vars/process.groovy
@@ -1,10 +1,10 @@
-def info(String message) {
+def info(message) {
     echo "INFO: ${message}"
 }
-
-def info() {
-    echo "INFO: "
-}
+//
+//def info() {
+//    echo "INFO: "
+//}
 
 def warning(message) {
     echo "WARNING: ${message}"
